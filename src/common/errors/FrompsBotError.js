655c69c392960177987f6848241db36913c8f5e4
Luciano L Silveira <llsilveira@llsilveira.com>
2022-03-26 09:13:49 -0300

    Created base Error class 'FrompsBotError'

---
+++ src/common/errors/FrompsBotError.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+module.exports = class FrompsBotError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = this.constructor.name;
+  }
+};
