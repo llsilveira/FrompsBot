86a638da1ccbba524eaca97aa358696a28ef47f6
Luciano L Silveira <llsilveira@llsilveira.com>
2022-04-09 10:03:29 -0300

    reorganized project directory structure

--- src/modules.js
+++ src/modules.js
@@ -4,7 +4,7 @@ const { loadConfig } = require("@fromps-bot/common/helpers");
 
 module.exports = {
   discord: {
-    require: "@fromps-bot/discord/Discord",
+    require: "@fromps-bot/modules/discord/Discord",
     config: loadConfig("discord")
   }
 };
