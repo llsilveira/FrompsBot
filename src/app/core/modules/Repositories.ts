cf33389ac9bde95094e703a681faf9304d49a3d1
Luciano L Silveira <llsilveira@llsilveira.com>
2022-09-30 10:49:34 -0300

    Created UserRepository

--- src/app/core/modules/Repositories.ts
+++ src/app/core/modules/Repositories.ts
@@ -2,13 +2,16 @@ import AppModule from "../../AppModule";
 
 import type Application from "../../Application";
 import UserAccountRepository from "../repositories/UserAccountRepository";
+import UserRepository from "../repositories/UserRepository";
 
 export default class Repositories extends AppModule {
   readonly userAccount: UserAccountRepository;
+  readonly user: UserRepository;
 
   constructor(app: Application) {
     super(app);
 
     this.userAccount = new UserAccountRepository(app.models.userAccount);
+    this.user = new UserRepository(app.models.user);
   }
 }
---
+++ src/app/core/repositories/UserRepository.ts
@@ -0,0 +1,5 @@
+import AppRepository from "../AppRepository";
+import { UserModel } from "../models/userModel";
+
+
+export default class UserRepository extends AppRepository<UserModel> {}
